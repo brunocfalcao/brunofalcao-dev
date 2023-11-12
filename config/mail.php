98033fd79d2067ca80e7b6a8411b2fac2da1cd07
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-11-13 00:47:02 +0100

    wip

--- config/mail.php
+++ config/mail.php
@@ -34,6 +34,11 @@ return [
     */
 
     'mailers' => [
+
+        'zeptomail' => [
+            'transport' => 'zeptomail',
+        ],
+
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
@@ -77,10 +82,6 @@ return [
                 'smtp',
                 'log',
             ],
-        ],
-
-        'zeptomail' => [
-            'transport' => 'zeptomail',
         ],
     ],
 
