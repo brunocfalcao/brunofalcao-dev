9078e4ca125561289be2b92da5602c817b24ad30
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2023-11-17 09:38:45 +0100

    Resovle merge conflict

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,28 @@
 | The Eduka route files are loaded dynamically by the framework.
 |
 */
+
+use Eduka\Cube\Models\Video;
+use Eduka\Nova\Actions\VimeoHandler;
+use Illuminate\Support\Arr;
+
+Route::get('y', function(){
+
+    $video = Video::first();
+
+    $courses = [];
+
+    $video->chapters->each(function($chapter) use(&$courses) {
+        $courses[] = $chapter->course_id;
+    });
+
+    dd($courses, array_unique($courses));
+
+    dd($video->chapters->unique('course_id'));
+
+    $x = (new VimeoHandler)->ensureProjectExists($video->course);
+    dd($x);
+    // $p = "/users/210982378/projects/18512233";
+    // dd(str($p)->afterLast('/')->toString());
+    return 'y;';
+});
