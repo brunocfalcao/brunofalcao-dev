78e2dba60affcf14b9687e1bd48480fd483641e4
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2023-11-19 09:21:48 +0100

    resolve merge confict

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,33 @@
 
 use Eduka\Cube\Models\Video;
 use Eduka\Nova\Actions\VimeoHandler;
+use Illuminate\Support\Arr;
+use Aws\S3\S3Client;
+use Aws\Credentials\Credentials;
+
+
+Route::get('/p', function () {
+    $applicationKeyId = env('BACKBLAZE_KEY_ID');
+    $applicationKey = env('BACKBLAZE_APP_KEY');
+    $bucketName = 'hello01';
+    $region = 'us-east-005'; // Replace with your B2 region code
+
+    $credentials = new Credentials($applicationKeyId, $applicationKey);
+
+    // Instantiate the S3 client using your B2profile
+    // Replace 'your_access_key', 'your_secret_key', and 'your_region' with your Backblaze B2 credentials
+    $client = new S3Client([
+        'version' => 'latest',
+        'region' => $region,
+        'endpoint' => 'https://s3.'.$region.'.backblazeb2.com',
+        'credentials' => $credentials,
+    ]);
+
+    // Create the bucket
+    return $client->createBucket([
+        'Bucket' => $bucketName,
+    ]);
+});
 
 Route::get('y', function () {
 
